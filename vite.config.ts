3230765da39da7db9368ecee38f58d38e15cc9f5
Sathishnaik786 <sathishnaik786@gmail.com>
2026-06-10 21:10:33 +0530

    fix: resolve hydration hang using SSG prerendering

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [
@@ -15,5 +16,6 @@ export default defineConfig({
         crawlLinks: true,
       },
     }),
+    react(),
   ],
 });
